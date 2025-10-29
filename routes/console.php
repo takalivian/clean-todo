b4156b329339bf8f95f57cdf8c90eb59964e3334
takamura <yoshitaka.00913@gmail.com>
2025-10-29 12:09:07 +0900

    ログの作成と削除

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// ログクリーンアップのスケジュール設定
+// 毎日午前2時に14日より古いログを削除
+Schedule::command('logs:clean', ['--days' => 14])
+    ->dailyAt('02:00')
+    ->timezone('Asia/Tokyo')
+    ->onSuccess(function () {
+        \Log::info('古いログの削除が完了しました');
+    })
+    ->onFailure(function () {
+        \Log::error('古いログの削除に失敗しました');
+    });
